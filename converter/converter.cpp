45ac515cbdb9a1c3af1459924bc94e56a9e8954f
Cimlah <28655852+Cimlah@users.noreply.github.com>
2022-03-20 23:33:24 +0100

    Moved tag recognition to a seperate function, added link recognition

--- converter/converter.cpp
+++ converter/converter.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 
 using namespace std;
 
+string tag_recognition(string checked_line) {
+    int same_character_counter=0;
+    regex link("\\[(.*)\\]\\((.*)\\)");
+
+    for(int i=0; i<checked_line.length(); i++) {
+        if(checked_line[i]=='#') same_character_counter++;
+    }
+
+    if(same_character_counter==1) return("H1");
+            else if(same_character_counter==2) return("H2");
+            else if(same_character_counter==3) return("H3");
+            else if(same_character_counter==4) return("H4");
+            else if(same_character_counter==5) return("H5");
+            else if(same_character_counter==6) return("H6");
+            else if(regex_match(checked_line, link)) return("Link");
+    else return("Unknown/Error");
+}
+
 int main() {
     ifstream markdown, html;
-    string checked_line, tags[2] = {"#"};
-    int same_character_counter=0;
+    string checked_line;
 
     markdown.open("index.md");
     html.open("index.html");
@@ -15,19 +33,7 @@ int main() {
     while(!markdown.eof()) {
         getline(markdown, checked_line);
 
-        for(int i=0; i<checked_line.length(); i++) {
-            if(checked_line[i]=='#') same_character_counter++;
-        }
-        
-        if(same_character_counter==1) {
-            cout << "H1" << endl;
-        }
-            else if(same_character_counter==2) {
-                cout << "H2" << endl;
-            }
-        else cout << "Something else" << endl;
-
-        same_character_counter=0;
+        cout << tag_recognition(checked_line) << endl;
     }
     
     return(0);
