5945744f0847a7b9c42c675a0247c497af01f2ad
Cimlah <28655852+Cimlah@users.noreply.github.com>
2022-03-26 21:53:19 +0100

    Wrote `to_italic` function to use smatch like in `to_link` Written function for all `h` tags
    Co-authored-by: lemmoor <lemmoor@users.noreply.github.com>

--- converter/converter.cpp
+++ converter/converter.cpp
@@ -5,34 +5,36 @@
 
 using namespace std;
 
-string to_italic(string text) {
-    regex italic_regex("(.*)\\*(.*)\\*(.*)"); //regex italic_regex("(.*)\\*(.*)\\*(.*)|(.*)_(.*)_(.*)");
+regex italic_regex("(.*)\\*(.*)\\*(.*)");
+regex full_link_regex("(.*)\\[(.*)\\]\\((.*)\\)(.*)");
 
-    int start_index = text.find("*");
-    string first = text.substr(0, start_index);
-    string italic_with_last = text.substr(start_index);
-    int end_index = italic_with_last.find("*", 1);
-    string italic = italic_with_last.substr(1, end_index-1);
-    string last = italic_with_last.substr(end_index+1);
+string to_italic(string checked_line) {
+    regex full_italic_regex("(.*)\\*(.*)\\*(.*)"); //regex italic_regex("(.*)\\*(.*)\\*(.*)|(.*)_(.*)_(.*)");
+    regex italic_regex("\\*(.*?)\\*");
+    regex line_start_regex("(.*?)\\*");
+    regex line_end_regex("\\*(.*)");
 
-    string converted = first + "<span class=\"italic\">" + italic + "</span>" + last;
+    string line_start = "";
+    string line_end = "";
 
-    if(regex_match(converted, italic_regex)) return(to_italic(converted));
+    smatch match;
+
+    regex_search(checked_line, match, italic_regex);
+    string italic = match.str(1);
+
+    if (regex_search(checked_line, match, line_start_regex) == true) {
+        line_start = match.str(1);
+    }
+
+    if (regex_search(checked_line, match, line_end_regex) == true) {
+        line_end = match.str(1);
+        line_end = line_end.substr(italic.length()+1);
+    }
+
+    string converted = line_start + "<span class=\"italic\">" + italic + "</span>" + line_end;
+    if(regex_match(converted, full_italic_regex)) return(to_italic(converted));
 
     return(converted);
-}
-
-string to_h1(string checked_line) {
-    regex italic_regex("(.*)\\*(.*)\\*(.*)");
-
-    string text = checked_line.substr(2);
-    string italic = text;
-
-    if(regex_match(checked_line, italic_regex)){
-      italic = to_italic(text);  
-    } 
-
-    return("<h1>" + italic + "</h1>");
 }
 
 string to_link(string checked_line){
@@ -40,8 +42,7 @@ string to_link(string checked_line){
     regex link_regex("\\((.*?)\\)");
     regex line_start_regex("(.*?)\\[");
     regex line_end_regex("\\)(.*)");
-    regex full_link_regex("(.*)\\[(.*)\\]\\((.*)\\)(.*)");
-
+    
     string line_start = "";
     string line_end = "";
 
@@ -68,6 +69,27 @@ string to_link(string checked_line){
     return(converted);
 }
 
+string to_h(string checked_line, int n) {
+    string text = checked_line.substr(n + 1);
+
+    if(regex_match(checked_line, italic_regex)){
+      text = to_italic(text);  
+    }
+
+    if(regex_match(checked_line, full_link_regex)) {
+        text = to_link(text);
+    }
+
+    char from_n =('0' + n);
+    string n_to_string = "";
+    n_to_string += from_n;
+
+
+    return("<h" + n_to_string + ">" + text + "</h" + n_to_string+ ">");
+}
+
+
+
 string tag_recognition(string checked_line) {
     int same_character_counter=0;
     regex link("(.*)\\[(.*)\\]\\((.*)\\)(.*)");
@@ -82,13 +104,13 @@ string tag_recognition(string checked_line) {
         if(checked_line[i]=='#') same_character_counter++;
     }
 
-    if(same_character_counter==1) return to_h1(checked_line);
+    if(same_character_counter==1) return to_h(checked_line, 1);
             // else if(regex_match(checked_line, italic)) tags+="italic ";
-            else if(same_character_counter==2) return("H2");
-            else if(same_character_counter==3) return("H3");
-            else if(same_character_counter==4) return("H4");
-            else if(same_character_counter==5) return("H5");
-            else if(same_character_counter==6) return("H6");
+            else if(same_character_counter==2) return to_h(checked_line, 2);
+            else if(same_character_counter==3) return to_h(checked_line, 3);
+            else if(same_character_counter==4) return to_h(checked_line, 4);
+            else if(same_character_counter==5) return to_h(checked_line, 5);
+            else if(same_character_counter==6) return to_h(checked_line, 6);
             else if(regex_match(checked_line, link)) return to_link(checked_line);
             else if(regex_match(checked_line, ordered_list)) return("Ordered list");
             else if(regex_match(checked_line, unordered_list)) return("Unordered list");
